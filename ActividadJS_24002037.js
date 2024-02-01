1f33762791666f6532511f9f15c0941180e21426
Gianluca Hernandez <24002037@galileo.edu>
2024-02-01 15:40:32 -0600

    first commit

---
+++ ActividadJS_24002037.js
@@ -0,0 +1,58 @@
+var mes = prompt("mes de nacimiento")
+var dia = prompt("dia de nacimiento")
+
+if(mes == 3 && dia >= 21 && dia <=31){
+  alert("Tu signo es Aries");
+}else if( mes == 4 && dia >= 1 && dia <=20){
+   alert("Tu signo es Aries");
+}else if( mes == 4 && dia >= 21 && dia <=30){
+   alert("Tu signo es Tauro");
+}else if( mes == 5 && dia >= 1 && dia <=20){
+   alert("Tu signo es Tauro");
+}else if( mes == 5 && dia >= 21 && dia <=31){
+   alert("Tu signo es Geminis");
+}else if( mes == 6 && dia >= 1 && dia <=20){
+   alert("Tu signo es Geminis");
+}else if( mes == 6 && dia >= 21 && dia <=30){
+   alert("Tu signo es Cancer");
+}else if( mes == 7 && dia >= 1 && dia <=22){
+   alert("Tu signo es Cancer");
+}else if( mes == 7 && dia >= 23 && dia <=31){
+   alert("Tu signo es Leo");
+}else if( mes == 8 && dia >= 1 && dia <=23){
+   alert("Tu signo esLeo");
+}else if( mes == 8 && dia >= 24 && dia <=31){
+   alert("Tu signo es Virgo");
+}else if( mes == 9 && dia >= 1 && dia <=22){
+   alert("Tu signo es Virgo");
+}else if( mes == 9 && dia >= 23 && dia <=30){
+   alert("Tu signo es Libra");
+}else if( mes == 10 && dia >= 1 && dia <=23){
+   alert("Tu signo es Libra");
+}else if( mes == 10 && dia >= 24 && dia <=31){
+   alert("Tu signo es Escorpio");
+}else if( mes == 11 && dia >= 1 && dia <=22){
+   alert("Tu signo es Escorpio");
+}else if( mes == 11 && dia >= 23 && dia <=30){
+   alert("Tu signo es Sagitario");
+}else if( mes == 12 && dia >= 1 && dia <=21){
+   alert("Tu signo es Sagitario");
+}else if( mes == 12 && dia >= 22 && dia <=31){
+   alert("Tu signo es Capricornio");
+}else if( mes == 1 && dia >= 1 && dia <=20){
+   alert("Tu signo es Capricornio");
+}else if( mes == 1 && dia >= 21 && dia <=31){
+   alert("Tu signo es Acuario");
+}else if( mes == 2 && dia >= 1 && dia <=19){
+   alert("Tu signo es Acuario");
+}else if( mes == 2 && dia >= 20 && dia <=28){
+   alert("Tu signo es Piscis");
+}else if( mes == 3 && dia >= 1 && dia <=20){
+   alert("Tu signo es Piscis");
+}else if( mes >= 13){
+   alert("Mes No valido")
+}else if( dia >= 32){
+   alert("Dia No valido")
+}else{
+  alert("Ingrese valores")
+}
